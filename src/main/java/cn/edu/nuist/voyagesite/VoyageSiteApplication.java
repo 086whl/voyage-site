e9c283e548b0da635b3da3aa2c2e956877f56583
whl <0086whl@gmail.com>
2021-11-06 21:54:08 +0800

    项目初始化

---
+++ src/main/java/cn/edu/nuist/voyagesite/VoyageSiteApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nuist.voyagesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyageSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoyageSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/nuist/voyagesite/VoyageSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.nuist.voyagesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

74cc8de9c83b62a2a2d42b46c581fc0ef9bb9c57
goatherd_YF <62420829@qq.com>
2021-11-07 17:25:35 +0800

    登录基本完成1.1

---
+++ src/main/java/cn/edu/nuist/voyagesite/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package cn.edu.nuist.voyagesite.mapper;
+
+import cn.edu.nuist.voyagesite.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+    void save(User user);
+
+    User findByUsernameAndPassword(String username, String password);
+
+
+}

0a65942a401f05a2d1727ee6038965f49956c193
086whl <0086whl@gmail.com>
2021-11-16 20:44:59 +0800

    旅游路线后台管理模块完成（缺批量删除）

--- src/main/java/cn/edu/nuist/voyagesite/controller/RouteController.java
+++ src/main/java/cn/edu/nuist/voyagesite/controller/RouteController.java
@@ -1,6 +1,7 @@
 package cn.edu.nuist.voyagesite.controller;
 
 import cn.edu.nuist.voyagesite.domain.Route;
+import cn.edu.nuist.voyagesite.domain.RutePackage;
 import cn.edu.nuist.voyagesite.service.RouteService;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Description: 路线列表后台管理
@@ -29,7 +32,10 @@ public class RouteController {
         return null;
     }
     @RequestMapping("/routeAdd")
-    public String routeAdd(){
+    public String routeAdd(@RequestParam("addJson") String addJson){
+        JSONObject userJson = JSONObject.parseObject(addJson);
+        Route routeAdd=JSON.toJavaObject(userJson,Route.class);
+        routeService.addRoute(routeAdd);
         return null;
     }
     @RequestMapping("/routeEdit")
@@ -38,6 +44,37 @@ public class RouteController {
         Route routeEdit=JSON.toJavaObject(userJson,Route.class);
         routeService.editRoute(routeEdit);
         return null;
+    }
+    @RequestMapping("/searchByRid")
+    public String searchByRid(@RequestParam("page") int page,@RequestParam("limit") int limit,@RequestParam("rid") int rid){
+        //分页
+        List<Route> searchByRid=routeService.searchByRid(((page-1)*limit),limit,rid);
+        int count=routeService.searchByRidCount(rid);
+        //封装
+        RutePackage rutePackage = new RutePackage();
+        rutePackage.setCode(0);
+        rutePackage.setMsg("success");
+        rutePackage.setData(searchByRid);
+        rutePackage.setCount(count);
+        //转为json
+        String allRouteList_J =JSON.toJSONString(rutePackage);
+
+        return allRouteList_J;
+    }
+    @RequestMapping("/searchByRname")
+    public String searchByRname(@RequestParam("page") int page,@RequestParam("limit") int limit,@RequestParam("rname") String rname){
+        //分页
+        List<Route> searchByRname=routeService.searchByRname(((page-1)*limit),limit,rname);
+        int count=routeService.searchByRnameCount(rname);
+        //封装
+        RutePackage rutePackage = new RutePackage();
+        rutePackage.setCode(0);
+        rutePackage.setMsg("success");
+        rutePackage.setData(searchByRname);
+        rutePackage.setCount(count);
+        //转为json
+        String allRouteList_J =JSON.toJSONString(rutePackage);
+        return allRouteList_J;
     }
 
 }
--- src/main/java/cn/edu/nuist/voyagesite/service/RouteService.java
+++ src/main/java/cn/edu/nuist/voyagesite/service/RouteService.java
@@ -22,7 +22,7 @@ public interface RouteService {
     //获取图片详情
     List<RouteImg> findRouteImageByRid(int rid);
     //获取所有路线总数
-    int routeAcount();
+    int routeCount();
     //获取所有页面（传入分页参数）
     List<Route> allRouteListPage(int page,int limit);
     //根据路线rid删除路线（逻辑删除rFlag=0）
@@ -31,4 +31,12 @@ public interface RouteService {
     void addRoute(Route route);
     //编辑路线
     void editRoute(Route route);
+    //通过rid查找路线,模糊查找（传入分页参数）
+    List<Route> searchByRid(int page,int limit,int rid);
+    //通过rid模糊查找后的记录总数
+    int searchByRidCount(int rid);
+    //通过路线名查找路线,模糊查找（传入分页参数）
+    List<Route> searchByRname(int page,int limit,String rname);
+    //通过路线名模糊查找后的记录总数
+    int searchByRnameCount(String rname);
 }
